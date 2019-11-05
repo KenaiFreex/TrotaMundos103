f5e7de10ece5d583fe5ca791a12127eab30a2516
Erick Torres <ericktorrez@kenaihub.com>
2019-11-05 17:50:10 +0000

    Adding some initial files for web structure

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Trotamundos estará en Línea pronto');
-})
- 
-app.listen(5000, ()=>{
-
-	console.log("Iniciando servidor en puerto 5000");
-
-
-});
+src/app.js
